cab854f0d49ec055636ebb65ef143dcb36fc3963
Rui <rui@dydx.exchange>
2023-07-01 04:36:25 -0700

    Clean up

--- cartera/src/main/java/exchange/dydx/cartera/walletprovider/providers/WalletConnectV2Provider.kt
+++ cartera/src/main/java/exchange/dydx/cartera/walletprovider/providers/WalletConnectV2Provider.kt
@@ -338,18 +338,11 @@ class WalletConnectV2Provider(
             val namespace = currentSession?.namespace()
             val chainId = currentSession?.chainId()
             val message = typedDataProvider?.typedDataAsString
-            val testMessage =   """
-            ["${account}","{\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallet\",\"type\":\"address\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person\"},{\"name\":\"contents\",\"type\":\"string\"}]},\"primaryType\":\"Mail\",\"domain\":{\"name\":\"Ether Mail\",\"version\":\"1\",\"chainId\":1,\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\"},\"message\":{\"from\":{\"name\":\"Cow\",\"wallet\":\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\"},\"to\":{\"name\":\"Bob\",\"wallet\":\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\"},\"contents\":\"Hello, Bob!\"}}"]
-        """.trimIndent()
-            val pa =  "[\"${account}\", ${message}]"
-            Log.d(tag(this@WalletConnectV2Provider), "sign: $testMessage")
             return if (sessionTopic != null && account != null && namespace != null && chainId != null && message != null) {
                 Sign.Params.Request(
                     sessionTopic = sessionTopic,
                     method = "eth_signTypedData",
                     params = "[\"${account}\", ${message}]",
-
-                    //params = testMessage,
                     chainId = "${namespace}:${chainId}"
                 )
             } else {

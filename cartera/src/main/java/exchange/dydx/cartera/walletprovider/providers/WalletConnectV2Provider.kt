ce85954d41aea5aeca2a74f50d60cb63f4cca88b
Rui <rui@dydx.exchange>
2023-08-11 12:38:45 -0700

    Clean up

--- cartera/src/main/java/exchange/dydx/cartera/walletprovider/providers/WalletConnectV2Provider.kt
+++ cartera/src/main/java/exchange/dydx/cartera/walletprovider/providers/WalletConnectV2Provider.kt
@@ -231,10 +231,6 @@ class WalletConnectV2Provider(
              SignClient.initialize(init) { error ->
                  Log.e(tag(this@WalletConnectV2Provider), error.throwable.stackTraceToString())
              }
-//
-//             PushDappClient.initialize(Push.Dapp.Params.Init(CoreClient, null)) { error ->
-//                 Log.e(tag(this), error.throwable.stackTraceToString())
-//             }
 
              SignClient.setDappDelegate(dappDelegate)
          }
@@ -336,9 +332,6 @@ class WalletConnectV2Provider(
             val namespace = currentSession?.namespace()
             val chainId = currentSession?.chainId()
             val message = typedDataProvider?.typedDataAsString
-
-           // val stringifiedParams =
-           //     """{\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallet\",\"type\":\"address\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\": \"to\",\"type\":\"Person\"},{\"name\":\"contents\",\"type\":\"string\"}]},\"primaryType\":\"Mail\",\"domain\":{\"name\":\"Ether Mail\",\"version\":\"1\",\"chainId\":1,\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\"},\"message\":{\"from\": {\"name\":\"Cow\",\"wallet\":\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\"},\"to\":{\"name\":\"Bob\",\"wallet\":\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\"},\"contents\":\"Hello, Bob!\"}}"""
 
             return if (sessionTopic != null && account != null && namespace != null && chainId != null && message != null) {
                 Sign.Params.Request(
